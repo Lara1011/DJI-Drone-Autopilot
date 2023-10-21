aad0337e581a9ece2b9c4e9ccead532cd045a83f
Lara Abu Hamad <laraabuhamad@gmail.com>
2023-10-21 19:57:11 +0300

    creating log class

--- app/src/main/java/com/dji/sdk/sample/demo/ILM/ILMInfoUpdate.java
+++ app/src/main/java/com/dji/sdk/sample/demo/ILM/ILMInfoUpdate.java
@@ -2,12 +2,22 @@ package com.dji.sdk.sample.demo.ILM;
 
 import static com.google.android.gms.internal.zzahn.runOnUiThread;
 
+import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.dji.sdk.sample.internal.controller.DJISampleApplication;
 import com.dji.sdk.sample.internal.utils.ModuleVerificationUtil;
+import com.dji.sdk.sample.internal.view.PresentableView;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -21,7 +31,7 @@ import dji.sdk.flightcontroller.FlightController;
 import dji.sdk.flighthub.model.FlightPathNode;
 import dji.sdk.gimbal.Gimbal;
 
-public class ILMInfoUpdate {
+public class ILMInfoUpdate implements PresentableView {
     private TextView Battery;
     private TextView x;
     private TextView y;
@@ -40,6 +50,17 @@ public class ILMInfoUpdate {
     private Handler locationUpdateHandler = new Handler();
     private Handler xyzUpdateHandler = new Handler();
 
+
+
+    private static final String TAG = "ILMStatusBar";
+    private static final String CSV_HEADER = "Date,Distance,Battery,Speed,X,Y,Z,Latitude,Longitude,Altitude,Roll,Pitch,Yaw";
+    private static final String CSV_FILENAME = "sdcard/droneLog/ILM_Log_File_aircraft_data.csv";
+
+    private Handler csvUpdateHandler = new Handler();
+    private File csvFile;
+    private FileWriter csvWriter;
+    private static final int CSV_UPDATE_INTERVAL_MS = 100; // Update every 100 milliseconds
+
     public ILMInfoUpdate(TextView battery, TextView x, TextView y, TextView z, TextView latitude, TextView longtitude, TextView altitude, TextView dateTime,
                          TextView speed, TextView distance, TextView pitch, TextView roll, TextView yaw) {
         this.Battery = battery;
@@ -55,7 +76,141 @@ public class ILMInfoUpdate {
         this.Pitch = pitch;
         this.Roll = roll;
         this.Yaw = yaw;
+        //createCsvFile();
+        //startCsvUpdateTask();
     }
+
+    public String getBattery() {
+        return Battery.getText().toString();
+    }
+
+
+    public String getILMX() {
+        return x.getText().toString();
+    }
+
+
+    public String getILMY() {
+        return y.getText().toString();
+    }
+
+
+    public String getILMZ() {
+        return z.getText().toString();
+    }
+
+
+    public String getLatitude() {
+        return (latitude.getText()).subSequence(10,latitude.getText().length()).toString();
+    }
+
+
+    public String getLongitude() {
+        return (longitude.getText()).subSequence(11,longitude.getText().length()).toString();
+    }
+
+
+    public String getAltitude() {
+        return (altitude.getText()).subSequence(10,altitude.getText().length()).toString();
+    }
+
+
+    public String getDate() {
+        return DateTime.getText().toString();
+    }
+
+
+    public String getSpeed() {
+        return Speed.getText().toString();
+    }
+
+
+    public String getDistance() {
+        return Distance.getText().toString();
+    }
+
+
+    public String getPitch() {
+        return Pitch.getText().toString();
+    }
+
+
+    public String getRoll() {
+        return Roll.getText().toString();
+    }
+
+
+    public String getYaw() {
+        return Yaw.getText().toString();
+    }
+
+
+    private void createCsvFile() {
+        try {
+            // Check if external storage is available
+            if (isExternalStorageWritable()) {
+                File dir = new File(Environment.getExternalStorageDirectory(), "AircraftData");
+                if (!dir.exists()) {
+                    if (!dir.mkdirs()) {
+                        Log.e(TAG, "Failed to create directory for CSV file");
+                        return;
+                    }
+                }
+                csvFile = new File(dir, CSV_FILENAME);
+                csvWriter = new FileWriter(csvFile,true);
+                if (csvFile.length() == 0) {
+                    csvWriter.append(CSV_HEADER);
+                    csvWriter.append("\n");
+                }
+                csvWriter.flush();
+            } else {
+                Log.e(TAG, "External storage is not writable");
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error creating CSV file: " + e.getMessage());
+        }
+    }
+
+
+
+
+
+    public void uploadToCsv() {
+        String data = getDate() + "," + getDistance() + "," + getBattery() + "," + getSpeed() + ","
+                + getILMX() + "," + getILMY() + "," + getILMZ() + "," + getLatitude() + ","
+                + getLongitude() + "," + getAltitude() + "," + getRoll() + "," + getPitch() + ","
+                + getYaw();
+
+
+        // Write the data to the CSV file
+        if (csvWriter != null) {
+            try {
+                csvWriter.append(data);
+                csvWriter.append("\n");
+                csvWriter.flush();
+            } catch (IOException e) {
+                Log.e(TAG, "Error writing to CSV file: " + e.getMessage());
+            }
+        }
+    }
+
+
+    private void startCsvUpdateTask() {
+        csvUpdateHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                uploadToCsv(); // Call the function to update the CSV file
+                csvUpdateHandler.postDelayed(this, CSV_UPDATE_INTERVAL_MS);
+            }
+        }, CSV_UPDATE_INTERVAL_MS);
+    }
+
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
 
     public void updateGimbalState() {
         if (ModuleVerificationUtil.isGimbalModuleAvailable()) {
@@ -83,7 +238,7 @@ public class ILMInfoUpdate {
                 if (DateTime != null) {
                     DateTime.setText(formattedDateTime);
                 }
-                //handler.postDelayed(this, 1000);
+                handler.postDelayed(this, 1000);
             }
         };
         updateTimeRunnable.run();
@@ -115,19 +270,18 @@ public class ILMInfoUpdate {
                         double lon = aircraftLocation.getLongitude();
                         double alt = aircraftLocation.getAltitude();
 
-                        latitude.setText(String.format(Locale.getDefault(), "%.6f", lat));
-                        longitude.setText(String.format(Locale.getDefault(), "%.6f", lon));
-                        altitude.setText(String.format(Locale.getDefault(), "%.6f", alt));
+                        latitude.setText("Latitude: "+String.format(Locale.getDefault(), "%.6f", lat));
+                        longitude.setText("Longitude: "+String.format(Locale.getDefault(), "%.6f", lon));
+                        altitude.setText("Altitude: "+String.format(Locale.getDefault(), "%.6f", alt));
                     }
                 }
-                //locationUpdateHandler.postDelayed(this, 1000);
+                locationUpdateHandler.postDelayed(this, 100);
             }
         };
         updateTimeRunnable.run();
     }
 
     public void updateXYZ() {
-        // Create a DecimalFormat object with two decimal places
         final DecimalFormat decimalFormat = new DecimalFormat("0.00000");
 
         if (ModuleVerificationUtil.isFlightControllerAvailable()) {
@@ -144,14 +298,12 @@ public class ILMInfoUpdate {
                         final String formattedVelocityY = decimalFormat.format(velocityY);
                         final String formattedVelocityZ = decimalFormat.format(velocityZ);
 
-                        // Update UI on the main thread
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                // Set the formatted velocity values in your UI elements
-                                x.setText(formattedVelocityX + " m/s");
-                                y.setText(formattedVelocityY + " m/s");
-                                z.setText(formattedVelocityZ + " m/s");
+                                x.setText(formattedVelocityX);
+                                y.setText(formattedVelocityY);
+                                z.setText(formattedVelocityZ);
                             }
                         });
                     }
@@ -161,15 +313,47 @@ public class ILMInfoUpdate {
     }
 
     public void updateSpeed() {
-        final Handler handler = new Handler();
-        Runnable updateSpeedRunnable = new Runnable() {
-            @Override
-            public void run() {
-                double speedValue = flightPathNode.getSpeed(); // Get the current speed
-                Speed.setText(speedValue + " m/s");
-                //handler.postDelayed(this, 1000); // Schedule this Runnable to run again after 1 second
+        final DecimalFormat decimalFormat = new DecimalFormat("0.0");
+
+        if (ModuleVerificationUtil.isFlightControllerAvailable()) {
+            DJISampleApplication.getAircraftInstance().getFlightController().setStateCallback(new FlightControllerState.Callback() {
+                @Override
+                public void onUpdate(FlightControllerState flightControllerState) {
+                    if (flightControllerState != null) {
+                        final float velocityX = flightControllerState.getVelocityX();
+                        final float velocityY = flightControllerState.getVelocityY();
+                        final float velocityZ = flightControllerState.getVelocityZ();
+                        final String speed = decimalFormat.format(Math.sqrt(velocityX*velocityX + velocityY*velocityY + velocityZ*velocityZ));
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Speed.setText(speed+"m/s");
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+
+    public void closeWriter(){
+        if (csvWriter != null) {
+            try {
+                csvWriter.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing CSV writer: " + e.getMessage());
             }
-        };
-        handler.post(updateSpeedRunnable);
+        }
+    }
+
+    @Override
+    public int getDescription() {
+        return 0;
+    }
+
+    @NonNull
+    @Override
+    public String getHint() {
+        return null;
     }
 }
